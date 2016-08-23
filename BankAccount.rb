c6710ac717ed79aa4a4b19e724e0ec1e3bbfbe6a
Rachel <rachel.l.pavilanis@gmail.com>
2016-08-23 12:52:16 -0700

    Added comments, started working on user class, caused withdrawal method and initialize method to raise ArgumentError if balance will go below 0.

--- BankAccount.rb
+++ BankAccount.rb
@@ -1,72 +1,75 @@
 
-
-# A new account cannot be created with initial negative balance - this will raise an ArgumentError (Google this)
-# The withdraw method does not allow the account to go negative - Will output a warning message and return the original un-modified balance
 # Bank Account - Wave 1
 # Rachel Pavilanis
 # August 23, 2016
 
 module Bank
 
+# class for bank accounts
   class Account
 
-    attr_accessor :id, :balance, :deposit_amount, :withdraw_amount
-
+    attr_accessor :deposit_amount, :withdraw_amount
+# initializes bank account with an ID and starting balance, returns Argument Error if proposed starting balance is not at or above 0.
     def initialize (account_hash)
       @id = account_hash[:id]
       @balance = account_hash[:balance]
+
+      unless @balance >= 0
+        raise ArgumentError.new("You need a balance above $0 to open your account.")
+      end
     end
 
+# formats numbers in a dollar format
     def change_two_decimals
       @balance = sprintf('%0.2f', @balance)  # => "550.50"
     end
-
+# displays the current balance, using the formatting from the change_two_decimals method
     def display_current_balance
       change_two_decimals
       puts "Your current balance is $#{@balance}."
     end
-
+# allows deposits to be made, and adds deposit amount to current balance
     def deposit (deposit_amount)
       @balance = deposit_amount.to_f + @balance.to_f
       display_current_balance
     end
-
+# allows withdrawals to be made, and subtracts deposit amount from current balance, unless
+# this amount would be less than 0. In that case, an error is returned.
     def withdraw (withdraw_amount)
       if @balance.to_f - withdraw_amount.to_f < 0
-        puts "You do not have enough money in your account to make that withdrawal."
+        raise ArgumentError.new("You do not have enough money in account to make that withdrawal.")
         display_current_balance
       else
         @balance = @balance.to_f - withdraw_amount.to_f
         display_current_balance
       end
     end
+  end
 
-account = Account.new(id: "45567", balance: 575.256)
+
+  class Owner
+
+    attr_accessor :first_name, :bank_ID, :street_address, :city, :state
+
+    def initialize (owner_hash)
+
+      @first_name = owner_hash[:first_name],
+      @bank_ID = owner_hash[:bank_ID],
+      @address = owner_hash[:street_address],
+      @city = owner_hash[:city],
+      @state = owner_hash[:state]
+  
+    end
+
+  end
+  end
+
+
+  end
+end
+
+account = Bank::Account.new(id: "45567", balance: 575.256)
 
 puts account.display_current_balance
 puts account.deposit(50.65)
 puts account.withdraw(700.00)
-
-
-
-
-  # class owner
-  #
-  #   attr_accessor :first_name, :bank_ID, :street_address, :city, :state
-  #
-  #   def initialize (owner_hash)
-  #
-  #     @first_name = owner_hash[:first_name],
-  #     @bank_ID = owner_hash[:bank_ID],
-  #     @address = owner_hash[:street_address],
-  #     @city = owner_hash[:city],
-  #     @state = owner_hash[:state]
-  #
-  #   end
-
-  # end
-  # end
-  #
-
-  end
-end
