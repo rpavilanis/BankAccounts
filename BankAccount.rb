a643af40ce02065911d9471859534a91c5e35d9a
Rachel <rachel.l.pavilanis@gmail.com>
2016-08-23 12:35:57 -0700

    Finished withdraw method and added if statement that does not allow account to go negative.

--- BankAccount.rb
+++ BankAccount.rb
@@ -1,5 +1,5 @@
 
-# Should have a withdraw method that accepts a single parameter which represents the amount of money that will be withdrawn. This method should return the updated account balance.
+
 # A new account cannot be created with initial negative balance - this will raise an ArgumentError (Google this)
 # The withdraw method does not allow the account to go negative - Will output a warning message and return the original un-modified balance
 # Bank Account - Wave 1
@@ -15,7 +15,6 @@ module Bank
     def initialize (account_hash)
       @id = account_hash[:id]
       @balance = account_hash[:balance]
-
     end
 
     def change_two_decimals
@@ -24,32 +23,29 @@ module Bank
 
     def display_current_balance
       change_two_decimals
-      puts "Your new balance is $#{@balance}."
+      puts "Your current balance is $#{@balance}."
     end
 
     def deposit (deposit_amount)
       @balance = deposit_amount.to_f + @balance.to_f
       display_current_balance
-
-      # user_input functionality
-      # puts "How much would you like to deposit today? Please enter a number in dollars and cents (e.g., 58.25)."
-      # @deposit_amount = gets.chomp
-      # until (@deposit_amount.is_a? Float)
-      #   puts "Please try again. Enter a number in dollars and cents."
-      #   @deposit_amount = gets.chomp #rescue nil
-      # end
     end
 
     def withdraw (withdraw_amount)
-      @balance = @balance.to_f - withdraw_amount.to_f
-      display_current_balance
+      if @balance.to_f - withdraw_amount.to_f < 0
+        puts "You do not have enough money in your account to make that withdrawal."
+        display_current_balance
+      else
+        @balance = @balance.to_f - withdraw_amount.to_f
+        display_current_balance
+      end
     end
 
 account = Account.new(id: "45567", balance: 575.256)
 
 puts account.display_current_balance
 puts account.deposit(50.65)
-puts account.withdraw(75.00)
+puts account.withdraw(700.00)
 
 
 
